94fad96a50e8bcc97a510c8e61df05762ace5961
dhoang110 <dung.hoang110@gmail.com>
2023-07-22 00:35:19 +0200

    update repository

--- atm_scraping.sql
+++ atm_scraping.sql
@@ -4,3 +4,28 @@ Create table best_atm(
 	trustscore float,
 	total_reviews integer,
 	domain varchar not null);
+
+Select * from best_atm;
+
+Create table company_details(
+	company_name varchar not null,
+	rating_class varchar,
+	star_5 varchar,
+	star_4 varchar,
+	star_3 varchar,
+	star_2 varchar,
+	star_1 varchar,
+	total_reviews varchar);
+
+Select * from company_details;
+
+Create table reviews(
+	company_name varchar not null,
+	review_star varchar,
+	rreview_title varchar,
+	reviewer_name varchar,
+	review_text varchar,
+	experience_date varchar,
+	review_date varchar,
+	reply_date varchar,
+	reply_text varchar);
