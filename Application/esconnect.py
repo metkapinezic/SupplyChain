57ce6941b62dca98e983b1d781926ea2df2b7b3a
dhoang110 <dung.hoang110@gmail.com>
2023-08-17 21:50:39 +0200

    new es-connect py

---
+++ Application/esconnect.py
@@ -0,0 +1,14 @@
+from elasticsearch import Elasticsearch, helpers
+import csv
+import warnings
+warnings.filterwarnings("ignore")
+
+# Connection to the cluster
+
+es = Elasticsearch(hosts = "https://elastic:datascientest@localhost:9200", 
+                    ca_certs="./ca/ca.crt")
+
+with open('app_reviews.csv', encoding='utf-8') as f:
+    reader = csv.DictReader(f)
+    helpers.bulk(es, reader, index='bestatm_reviews')
+
