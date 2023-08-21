343fe8f01e037e9aa34e1c114522745d8241e73c
dhoang110 <dung.hoang110@gmail.com>
2023-08-22 00:01:57 +0200

    edit the repo and add dockercompose

--- CMD.sh
+++ CMD.sh
@@ -4,27 +4,10 @@ docker-compose up -d
 sleep 240
 
 
-#step 1 webscrapping
+#run the application
 cd ..
 cd Application
 docker cp elasticsearch-es01-1:usr/share/elasticsearch/config/certs/ca/ ./
-
-# we just have to do docker-compose again and docker-compose up -d, no need to specify which file to run
-run mainpage.py
-run subpage.py
-
-#step 3 transformation
-run transformation.py
-
-#step 4 push data to elasticsearch
-run esconnect.py
-
-#step 5 run sentiment analysis
-run sentiment.py
-
-#step 6 run dash
-pip install dash
-run dashboard.py
-
+docker-compose up -d
 
 #dash runs on http://localhost:8050/ 
