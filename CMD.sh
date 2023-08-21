8d82dd324e9e50438bc3eca047c12077bd875cbe
dhoang110 <dung.hoang110@gmail.com>
2023-08-22 00:00:22 +0200

    edit the repo and add dockercompose

--- CMD.sh
+++ CMD.sh
@@ -1,10 +1,15 @@
 #connect to elasticsearch
 cd elasticsearch
 docker-compose up -d
+sleep 240
+
+
+#step 1 webscrapping
+cd ..
 cd Application
 docker cp elasticsearch-es01-1:usr/share/elasticsearch/config/certs/ca/ ./
 
-#step 1 webscrapping
+# we just have to do docker-compose again and docker-compose up -d, no need to specify which file to run
 run mainpage.py
 run subpage.py
 
